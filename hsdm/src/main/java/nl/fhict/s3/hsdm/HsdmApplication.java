3b24bb1d7010e5f6782fe11b9fc941819c92d10e
SandersonMeulensteen <63857652+SanderMeulensteen@users.noreply.github.com>
2022-09-27 13:33:39 +0200

    project opgezet

---
+++ hsdm/src/main/java/nl/fhict/s3/hsdm/HsdmApplication.java
@@ -0,0 +1,13 @@
+package nl.fhict.s3.hsdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsdmApplication.class, args);
+	}
+
+}
---
+++ hsdm/src/test/java/nl/fhict/s3/hsdm/HsdmApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fhict.s3.hsdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
